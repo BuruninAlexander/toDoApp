3f53a872e69db502b450e7ded86054db021dd3c9
Burunin Alexander <horosheeslovo@icloud.com>
2022-01-13 12:13:59 +0300

    1

--- src/App.js
+++ src/App.js
@@ -7,43 +7,38 @@ import MyInput from './Ui/Input/MyInput';
 
 
 function App() {
-  const [posts, setPost] = useState(
+  const [posts, setPosts] = useState(
     [{ id: 1, title: "javascript", body: 'Description' },
     { id: 4, title: "javascript", body: 'Description' },
     { id: 2, title: "javascript", body: 'Description' },
     { id: 3, title: "javascript", body: 'Description' },
     ]
   )
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
-  console.log(title)
+  const [post, setPost] = useState({ body: " ", tytle: " " })
+  // const [body, setBody] = useState('')
+  console.log(post)
 
   const addNewPOst = (e) => {
     e.preventDefault()
-    const newPost = {
-      id: Date.now(),
-      title,
-      body
-    }
-    setPost([...posts, newPost])
-    setTitle("")
-    setBody("")
+
+
+    setPosts([...posts, { ...post, id: Date.now() }])
+    setPost({ title: " ", body: " " })
 
   }
-
   return (
     <div className="App">
       <form>
         {/* управляемый компонент  */}
         <MyInput
-          value={title}
-          onChange={e => setTitle(e.target.value)}
+          value={post.title}
+          onChange={e => setPost({ ...post, title: e.target.value })}
           type="text"
           placeholder="описание загаловка">
         </MyInput>
         <MyInput
-          value={body}
-          onChange={e => setBody(e.target.value)}
+          value={post.body}
+          onChange={e => setPost({ ...post, body: e.target.value })}
           type="text"
           placeholder="описание поста">
 
